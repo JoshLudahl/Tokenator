86ec77df93525039c3c100f8c4d9efccfd29032e
Joshy <josh.ludahl@gmail.com>
2021-01-06 21:00:45 -0800

    minor changes

--- app/src/main/java/com/token/tokenator/savedpassword/SavedPasswordAdapter.kt
+++ app/src/main/java/com/token/tokenator/savedpassword/SavedPasswordAdapter.kt
@@ -2,7 +2,6 @@ package com.token.tokenator.savedpassword
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.token.tokenator.databinding.LayoutSavedTokenListItemBinding
 import com.token.tokenator.model.Token
@@ -20,9 +19,6 @@ class SavedPasswordAdapter : RecyclerView.Adapter<SavedPasswordAdapter.ViewHolde
             itemBinding.apply {
                 tokenTitle.text = token.title
                 tokenPlaceholder.text = token.token
-            }
-            itemBinding.copyIcon.setOnClickListener {
-               // copy to clipboard
             }
         }
     }
