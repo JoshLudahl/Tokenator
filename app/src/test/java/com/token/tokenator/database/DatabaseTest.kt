82f9c9464b4b9b6d38587c54269e86ef371f2585
Joshy <josh.ludahl@gmail.com>
2021-01-09 20:32:17 -0800

    basic class setup to test database

---
+++ app/src/test/java/com/token/tokenator/database/DatabaseTest.kt
@@ -0,0 +1,23 @@
+package com.token.tokenator.database
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class DatabaseTest {
+
+    @Before
+    fun setup() {
+
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun canInsertIntoDatabase() {
+
+    }
+}
