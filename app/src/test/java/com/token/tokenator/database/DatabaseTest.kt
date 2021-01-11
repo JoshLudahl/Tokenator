341d34079cd8ab6623f2d3366b86c7c532ad9f18
Joshy <josh.ludahl@gmail.com>
2021-01-10 21:43:41 -0800

    tests

--- app/src/test/java/com/token/tokenator/database/DatabaseTest.kt
+++ app/src/test/java/com/token/tokenator/database/DatabaseTest.kt
@@ -1,23 +1,48 @@
 package com.token.tokenator.database
 
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.token.tokenator.model.Token
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
 
 class DatabaseTest {
 
-    @BeforeEach
-    fun setup() {
+//    private lateinit var tokenDao: TokenDao
+//    private lateinit var db: TokenDatabase
+//
+//    private val TOKEN = "234LKFDSA;LK,M23sdfakl78"
 
+    @Before
+    fun setup() {
+//        val context: Context = ApplicationProvider.getApplicationContext()
+//
+//        db = Room.inMemoryDatabaseBuilder(context, TokenDatabase::class.java)
+//            .allowMainThreadQueries()
+//            .build()
+//        tokenDao = db.tokenDao()
     }
 
-    @AfterEach
-    fun tearDown() {
-
+    @After
+    fun closeDatabase() {
+//        db.close()
     }
 
     @Test
-    fun canInsertIntoDatabase() {
-
+    @Throws(Exception::class)
+    fun canInsertIntoDatabase() = runBlocking {
+//        val token = Token(
+//            title = "Super Secret",
+//            token = TOKEN
+//        )
+//        tokenDao.insert(token)
+//        val allTokens = tokenDao.getAllTokens().value?.first()
+//        assert(allTokens == token)
     }
 }
