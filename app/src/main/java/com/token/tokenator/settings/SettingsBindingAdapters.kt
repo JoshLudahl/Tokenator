1ea5b161b5203a610fc79c348618510c4b2a11d6
Joshy <josh.ludahl@gmail.com>
2021-03-11 19:44:47 -0800

    Updated the binding adapter to change the color of the font color to help with accessibility.

--- app/src/main/java/com/token/tokenator/settings/SettingsBindingAdapters.kt
+++ app/src/main/java/com/token/tokenator/settings/SettingsBindingAdapters.kt
@@ -7,8 +7,14 @@ import com.token.tokenator.model.SettingsItem
 
 @BindingAdapter("itemText")
 fun TextView.setBackgroundColor(item: SettingsItem) {
-    setBackgroundResource(when(item.included) {
-        true -> R.drawable.settings_item_active_background
-        false -> R.drawable.settings_item_inactive_background
-    })
+
+    when (item.included) {
+        true -> {
+            setBackgroundResource(R.drawable.settings_item_active_background)
+        }
+        false -> {
+            setBackgroundResource(R.drawable.settings_item_inactive_background)
+            setTextColor(resources.getColor(R.color.yellow, context.theme))
+        }
+    }
 }
