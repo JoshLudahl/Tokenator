e394221568902f2d5eb4ee81a30bcc604c3bf668
Joshy <josh.ludahl@gmail.com>
2021-03-22 12:56:45 -0700

    Added some tests and updated the build gradle file for animations and updated the testing library version.

--- app/src/androidTest/java/com/token/tokenator/screens/MainScreen.kt
+++ app/src/androidTest/java/com/token/tokenator/screens/MainScreen.kt
@@ -1,23 +1,38 @@
 package com.token.tokenator.screens
 
-import com.android.elk.espresso.click
-import com.android.elk.espresso.on
-import com.android.elk.espresso.view
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.platform.app.InstrumentationRegistry
+import com.android.elk.common.stringValue
+import com.android.elk.common.targetContext
+import com.android.elk.espresso.*
+import com.google.android.material.internal.ContextUtils
 import com.token.tokenator.R
 import com.token.tokenator.model.Type
+import org.hamcrest.Matchers
+import org.hamcrest.core.IsNot
 
 class MainScreen {
 
     fun selectSettingsButton() {
-
+        click on view(R.id.settings_button)
     }
 
     fun selectBookmarkButton() {
-
+        click on view(R.id.view_saved_button)
     }
 
-    fun selectSwitch(type: Type) {
-
+    fun selectSwitch(vararg types: Type) {
+        for (type in types) {
+            click on when (type) {
+                Type.LOWERCASE -> R.id.switch_lower_case
+                Type.NUMERIC -> R.id.switch_numeric
+                Type.SPECIAL -> R.id.switch_special_characters
+                Type.UPPERCASE -> R.id.switch_uppercase
+            }.let { view(it) }
+        }
     }
 
     fun dragLengthButton() {
@@ -28,7 +43,22 @@ class MainScreen {
         click on view(R.id.button_generate_token)
     }
 
-    fun generatePasswordFieldIsNotEmpty() {
+    fun generateToastIsDisplayed() {
+        toastMatcher(R.string.toast_copied_to_clipboard)
+    }
 
+    fun generatePasswordFieldIsNotEmpty() {
+        view(R.id.generated_field) confirm view("").not()
+    }
+
+    fun enterTextIntoPasswordNameField(text: String) {
+        typeText(text) into view(R.id.token_name)
+    }
+
+    fun selectSavedPasswordField() {
+        with(view(R.id.save_button)) {
+            scrollTo()
+            click()
+        }
     }
 }
--- app/src/androidTest/java/com/token/tokenator/tests/MainScreenTest.kt
+++ app/src/androidTest/java/com/token/tokenator/tests/MainScreenTest.kt
@@ -1,21 +1,65 @@
 package com.token.tokenator.tests
 
+import com.android.elk.common.Given
+import com.android.elk.common.Then
+import com.android.elk.common.When
 import com.android.elk.screen
 import com.android.elk.verify
 import com.token.tokenator.BaseTest
+import com.token.tokenator.model.Type
 import com.token.tokenator.screens.MainScreen
 import org.junit.Test
 
 class MainScreenTest: BaseTest() {
 
     @Test
-    fun selectGenerateButtonGeneratesAToken() {
+    fun clickingSettingsGoesToSettingsView() {
+        screen<MainScreen> {
+            selectSettingsButton()
+        }
+        // TODO: 3/22/21 Add verification
+    }
+
+    @Test
+    fun clickingBookmarkIconGoesToSavedPasswords() {
+        screen<MainScreen> {
+            selectBookmarkButton()
+        }
+        // TODO: 3/22/21 Add verification
+    }
+
+    @Test
+    fun selectGenerateButtonGeneratesATokenAndDisplaysAToast() {
         screen<MainScreen> {
             selectGenerate()
-            Thread.sleep(3000)
         }
         verify<MainScreen> {
-
+            generateToastIsDisplayed()
+            generatePasswordFieldIsNotEmpty()
         }
+    }
+
+    @Test
+    fun selectEachSwitch() {
+        screen<MainScreen> {
+            selectSwitch(*Type.values())
+        }
+    }
+
+    @Test
+    fun generatePasswordThenEnterNameThenSave() {
+        @Given("That I see the generate password button")
+        @When("I select the generate button")
+        screen<MainScreen> {
+            selectGenerate()
+            enterTextIntoPasswordNameField("Generic Name")
+            selectSavedPasswordField()
+        }
+
+        @Then("I see my password saved in the save password screen")
+        screen<MainScreen> {
+            selectBookmarkButton()
+        }
+        // TODO: 3/22/21 Add verification
     }
 }
