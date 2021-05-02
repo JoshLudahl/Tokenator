6f61bccc88b1f3a9024ef9b8d356fa9dfb9d169e
Joshy <josh.ludahl@gmail.com>
2021-05-01 17:33:52 -0700

    Updated strings and formatting.

--- app/src/main/java/com/token/tokenator/ui/savedpassword/SavedPasswordAdapter.kt
+++ app/src/main/java/com/token/tokenator/ui/savedpassword/SavedPasswordAdapter.kt
@@ -49,7 +49,6 @@ class SavedPasswordAdapter(private val clickListener: TokenListener) :
                                 }
                                 this.text = Encryption.decrypt(it)
                             }
-
                         }
                     }
                     else -> {
