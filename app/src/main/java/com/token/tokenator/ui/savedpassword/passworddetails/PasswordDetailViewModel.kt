4137d24a3e9d9641ac2b07d9c067b86277fb216c
Joshy <josh.ludahl@gmail.com>
2021-05-16 19:43:08 -0700

    Added DI to detail view

--- app/src/main/java/com/token/tokenator/ui/savedpassword/passworddetails/PasswordDetailViewModel.kt
+++ app/src/main/java/com/token/tokenator/ui/savedpassword/passworddetails/PasswordDetailViewModel.kt
@@ -1,7 +1,14 @@
 package com.token.tokenator.ui.savedpassword.passworddetails
 
 import androidx.lifecycle.ViewModel
+import com.token.tokenator.database.token.TokenRepository
+import com.token.tokenator.model.Token
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class PasswordDetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class PasswordDetailViewModel @Inject constructor(
+    private val tokenRepository: TokenRepository
+) : ViewModel() {
+
 }
