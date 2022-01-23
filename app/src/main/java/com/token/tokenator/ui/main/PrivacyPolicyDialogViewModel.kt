35aa5b28403588e302014794b98e545d1fb8e7cf
Joshy <josh.ludahl@gmail.com>
2022-01-22 23:12:21 -0800

    Added Privacy Policy dialog

--- app/src/main/java/com/token/tokenator/ui/main/PrivacyPolicyDialogViewModel.kt
+++ app/src/main/java/com/token/tokenator/ui/main/PrivacyPolicyDialogViewModel.kt
@@ -1,17 +1,20 @@
 package com.token.tokenator.ui.main
 
-import android.view.View
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.ViewModel
+import com.token.tokenator.R
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 class PrivacyPolicyDialogViewModel : ViewModel(), LifecycleObserver {
 
-    private val _shouldDismiss = MutableStateFlow<Boolean>(false)
+    private val _shouldDismiss = MutableStateFlow(false)
 
     val shouldDismiss: StateFlow<Boolean>
         get() = _shouldDismiss
+
+    private val _titleText = R.string.privacy_policy
+    val titleText get() = _titleText
 
     fun dismissDialog() {
         _shouldDismiss.value = true
