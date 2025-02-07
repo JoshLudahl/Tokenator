8e373e3ac5bb57e528315efe05ccedc08e4b896b
joshy <josh.ludahl@gmail.com>
2025-02-07 15:22:13 -0800

    feat: Add in-app update functionality
    This commit adds in-app update functionality to the application.
    
    Changes include:
    
    -   Added dependencies for `app-update` and `app-update-ktx` in `libs.versions.toml` and `build.gradle`.
    -   Implemented the `AppUpdateManager` and related components in `MainActivity.kt` to check for available updates and start the update flow.
    -   Implemented `InstallStateUpdatedListener` in `MainActivity.kt` to handle download completion and restarting the app.
    - Configured `AppUpdateOptions` to use `AppUpdateType.FLEXIBLE`.

--- app/src/main/java/com/token/tokenator/MainActivity.kt
+++ app/src/main/java/com/token/tokenator/MainActivity.kt
@@ -1,26 +1,92 @@
 package com.token.tokenator
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.gms.tasks.Task
+import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.token.tokenator.R.id.myNavHostFragment
 import com.token.tokenator.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
+    private lateinit var appUpdateManager: AppUpdateManager
+
+    private lateinit var aut: Task<AppUpdateInfo>
+    private val updateType = AppUpdateType.FLEXIBLE
+
+    val listener =
+        InstallStateUpdatedListener { state ->
+            if (state.installStatus() == InstallStatus.DOWNLOADED) {
+
+                Log.i("MainActivity", "Update has been downloaded.")
+                Toast.makeText(
+                    this,
+                    "Update Completed. Restarting application.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+                lifecycleScope.launch {
+                    appUpdateManager.completeUpdate()
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        appUpdateManager = AppUpdateManagerFactory.create(applicationContext)
+        aut = appUpdateManager.appUpdateInfo
+        checkIsUpdateAvailable()
         setTheme(R.style.Theme_Tokenator)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         navHostFragment =
             supportFragmentManager.findFragmentById(myNavHostFragment) as NavHostFragment
         navController = navHostFragment.navController
+    }
+
+    private fun checkIsUpdateAvailable() {
+        val activityResultLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.StartIntentSenderForResult(),
+            ) { result ->
+                if (result.resultCode != RESULT_OK) {
+                    Log.i("MainActivity", "The Update has failed.")
+                }
+            }
+
+        aut.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE &&
+                appUpdateInfo.isUpdateTypeAllowed(updateType)
+            ) {
+                Log.i("MainActivity", "Update is available.")
+
+                appUpdateManager.registerListener(listener)
+                Log.i("MainActivity", "Starting Update.")
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    activityResultLauncher,
+                    AppUpdateOptions.newBuilder(updateType).build(),
+                )
+            } else {
+                Log.i("MainActivity", "No Update Available.")
+            }
+        }
     }
 }
