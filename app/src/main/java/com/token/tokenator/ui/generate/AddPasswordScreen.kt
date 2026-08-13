405f0c3cede090e0eea8287c10ae300623f62500
joshy <josh.ludahl@gmail.com>
2026-08-13 13:23:12 -0700

    auto-agent: saved

--- app/src/main/java/com/token/tokenator/ui/generate/AddPasswordScreen.kt
+++ app/src/main/java/com/token/tokenator/ui/generate/AddPasswordScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
