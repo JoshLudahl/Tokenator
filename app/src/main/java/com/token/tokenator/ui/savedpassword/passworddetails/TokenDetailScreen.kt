a87508a57700e4723be628a1f475afda4383010a
joshy <josh.ludahl@gmail.com>
2026-08-15 09:50:07 -0700

    auto-agent: formatting

--- app/src/main/java/com/token/tokenator/ui/savedpassword/passworddetails/TokenDetailScreen.kt
+++ app/src/main/java/com/token/tokenator/ui/savedpassword/passworddetails/TokenDetailScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -131,6 +132,7 @@ fun TokenDetailScreen(
                     .padding(paddingValues)
                     .fillMaxSize()
                     .verticalScroll(rememberScrollState())
+                    .imePadding()
                     .padding(24.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
